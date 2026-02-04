cc96a0c0828843512c1e7fe0709fdecb83722df1
zhenqiiii <zhangwenyang2004@foxmail.com>
2026-02-04 14:04:48 +0800

    ✨ feat(main): 接入rag服务到后端
    将pipeline注入到处理函数中，后续只需专注后端编写即可

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"go-rag/api"
 	"go-rag/config"
+	"go-rag/pkg/embedding"
+	"go-rag/pkg/llm"
+	"go-rag/rag"
 	"log"
-	"strconv"
 )
 
 func main() {
@@ -14,74 +17,50 @@ func main() {
 		log.Fatalf("加载配置失败： %v", err)
 	}
 
+	// TODO:初始化数据库
+
 	// 启动RAG服务
 	// 根据config文件配置好pipeline
 	// 然后注入到后端路由中供路由处理函数调用
 
-	// 启动后端服务
-	r := api.SetupRouter()
+	// 两个API客户端:embedding & llm
+	embeddingClient := embedding.NewEmbeddingClient(
+		config.EmbeddingAPI.BaseURL,
+		config.EmbeddingAPI.APIKey,
+		config.EmbeddingAPI.Model,
+	)
+	llmClient := llm.NewLLMClient(
+		config.LLMAPI.BaseURL,
+		config.LLMAPI.APIKey,
+		config.LLMAPI.Model,
+	)
+
+	// 先创建store组件和embedder组件(retriever组件要复用，保证组件实例只有一个)
+	store := rag.NewQdrantStore(
+		config.Qdrant.Host,
+		config.Qdrant.Port,
+		config.Qdrant.Collection,
+		config.Qdrant.Dimension,
+	)
+	// 初始化store组件（不过这样的话似乎pipeline的Init方法就多余了，之前没考虑到）
+	if err := store.Init(); err != nil {
+		log.Fatalf("初始化向量库失败：%v", err)
+	}
+
+	// embedder
+	embedder := rag.NewAPIEmbedder(embeddingClient, 10)
+
+	// 创建pipeline
+	pipeline := rag.NewRAGPipeline(
+		rag.NewTextChunker(config.RAG.ChunkSize, config.RAG.ChunkOverlap),
+		embedder,
+		store,
+		rag.NewVectorRetriever(store, embedder, float32(config.RAG.ScoreThreshold)),
+		rag.NewLLMGenerator(llmClient),
+	)
+
+	// 启动后端服务并注入rag服务和数据库（TODO）
+	r := api.SetupRouter(pipeline)
 	// 小马快跑 🐎💨
-	r.Run(strconv.Itoa(config.Server.Port))
-
-	// // RAGPipeline整体测试
-	// // chunker
-	// chunker := rag.NewTextChunker(300, 20)
-
-	// // embedder
-	// client := embedding.NewEmbeddingClient(config.EmbeddingAPI.BaseURL, config.EmbeddingAPI.APIKey, config.EmbeddingAPI.Model)
-	// embedder := rag.NewAPIEmbedder(client, 10)
-
-	// // store
-	// store := rag.NewQdrantStore(config.Qdrant.Host, config.Qdrant.Port, config.Qdrant.Collection, config.Qdrant.Dimension)
-
-	// // retriever
-	// retriever := rag.NewVectorRetriever(store, embedder, 0.5)
-
-	// // generator
-	// llmClient := llm.NewLLMClient(
-	// 	config.LLMAPI.BaseURL,
-	// 	config.LLMAPI.APIKey,
-	// 	config.LLMAPI.Model,
-	// )
-	// generator := rag.NewLLMGenerator(llmClient)
-
-	// // 创建流水线
-	// testPipeline := rag.NewRAGPipeline(
-	// 	chunker,
-	// 	embedder,
-	// 	store,
-	// 	retriever,
-	// 	generator,
-	// )
-	// // Init
-	// testPipeline.Init()
-
-	// // 读取AI.txt文件
-	// content, err := os.ReadFile("AI.txt")
-	// if err != nil {
-	// 	log.Fatalf("文件读取错误: %v", err)
-	// }
-	// document := &models.Document{
-	// 	ID:        "321",
-	// 	Filename:  "AI",
-	// 	Content:   string(content),
-	// 	CreatedAt: time.Now(),
-	// }
-
-	// // 上传文件
-	// IdxResult, err := testPipeline.IndexDocument(document)
-	// if err != nil {
-	// 	log.Fatalf("上传文件失败: %v", err)
-	// }
-	// fmt.Println(IdxResult)
-
-	// 问答
-	// query := "人工智能伦理的核心原则有哪些，每条都简单说一说"
-	// response, err := testPipeline.Query(query, 10)
-	// if err != nil {
-	// 	log.Fatalf("回答生成失败: %v", err)
-	// }
-
-	// fmt.Printf("%+v", response)
-
+	r.Run(fmt.Sprintf(":%d", config.Server.Port))
 }
